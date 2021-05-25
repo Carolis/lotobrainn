c4634675679f6266e63c32aeb6208b4e9f1ed787
Carol <redrumjedi@gmail.com>
2021-05-25 19:24:27 -0300

    add Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <span>
+        Este sorteio é meramente ilustrativo e não possui nenhuma ligação com a
+        CAIXA.
+      </span>
+    </footer>
+  )
+}
+
+export default Footer
