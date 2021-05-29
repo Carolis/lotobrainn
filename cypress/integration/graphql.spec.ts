8e1eb1b166ac3b9d2049d5f0c98f32c7f99e0e84
Carol <redrumjedi@gmail.com>
2021-05-29 18:49:34 -0300

    add first query fetch test

--- cypress/integration/graphql.spec.ts
+++ cypress/integration/graphql.spec.ts
@@ -1,1 +1,21 @@
 /// <reference types="cypress" />
+
+describe('Get Lotteries query', () => {
+  it('Should fetch lotteries data', () => {
+    const getLotteriesString = `{
+          loterias {
+              id
+              nome
+          }
+      }`
+    cy.request({
+      method: 'POST',
+      url: 'https://brainn-api-loterias.herokuapp.com/graphql',
+      body: {
+        query: getLotteriesString
+      }
+    }).then((res) => {
+      cy.log(res.body)
+    })
+  })
+})
