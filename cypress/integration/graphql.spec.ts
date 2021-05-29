5a583640920bc57ac754be48794dce3d65e1b1f5
Carol <redrumjedi@gmail.com>
2021-05-29 19:12:49 -0300

    add remaining queries tests

--- cypress/integration/graphql.spec.ts
+++ cypress/integration/graphql.spec.ts
@@ -18,4 +18,42 @@ describe('Get Lotteries query', () => {
       cy.log(res.body)
     })
   })
+
+  it('Should fetch lotteries and bets data', () => {
+    const getLotteriesBetsString = `{
+      loteriasConcursos {
+        loteriaId
+        concursoId
+      }
+    }`
+    cy.request({
+      method: 'POST',
+      url: 'https://brainn-api-loterias.herokuapp.com/graphql',
+      body: {
+        query: getLotteriesBetsString
+      }
+    }).then((res) => {
+      cy.log(res.body)
+    })
+  })
+
+  it('Should fetch bets info data', () => {
+    const getBetsString = `{
+      concurso(id: 0) {
+        id
+        loteria
+        numeros
+        data
+      }
+    }`
+    cy.request({
+      method: 'POST',
+      url: 'https://brainn-api-loterias.herokuapp.com/graphql',
+      body: {
+        query: getBetsString
+      }
+    }).then((res) => {
+      cy.log(res.body)
+    })
+  })
 })
